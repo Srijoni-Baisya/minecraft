c9400a8c49b913045f73accfaaf1817f8898beb6
Srijoni-Baisya <75311639+Srijoni-Baisya@users.noreply.github.com>
2021-04-02 17:52:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -58,22 +58,22 @@ function my_keydown(e){
     }
 
     if(keyPressed=='37'){
-       //left();
+       left();
        console.log("left");
     }
 
     if(keyPressed=='39'){
-        //right();
+        right();
         console.log("right");
     }
 
     if(keyPressed=='38'){
-       // up();
+        up();
         console.log("up");
     }
 
     if(keyPressed=='40'){
-       // down();
+        down();
         console.log("down");
     }
 
@@ -120,5 +120,45 @@ function my_keydown(e){
     if(keyPressed=='82'){
         newImage("roof.jpg");
         console.log("r");
+    }
+}
+
+function up(){
+    if(player_y>0){
+        player_y= player_y-block_image_height;
+        console.log("block image height = "+block_image_height);
+        console.log("When Up arrow key is pressed, x = "+player_x+" , y = "+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function down(){
+    if(player_y<460){
+        player_y= player_y+block_image_height;
+        console.log("block image height = "+block_image_height);
+        console.log("When Down arrow key is pressed, x = "+player_x+" , y = "+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function left(){
+    if(player_x>0){
+        player_x= player_x-block_image_width;
+        console.log("block image width = "+block_image_width);
+        console.log("When Left arrow key is pressed, x = "+player_x+" , y = "+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function right(){
+    if(player_x<850){
+        player_x= player_x+block_image_width;
+        console.log("block image width = "+block_image_width);
+        console.log("When Right arrow key is pressed, x = "+player_x+" , y = "+player_y);
+        canvas.remove(player_object);
+        player_update();
     }
 }
